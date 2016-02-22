e15fd6ac87a6f73b6da9f684125508dd8de48424
qstin <justinjeffreyp@gmail.com>
2016-02-22 13:19:30 -0700

    update code to complete tasks

--- hw1-handout/test.js
+++ hw1-handout/test.js
@@ -23,7 +23,7 @@ var easingsList = [
 "easeInOutCirc",
 "easeInElasic",
 "easeOutElastic",
-"easeInOutElatic",
+"easeInOutElastic",
 "easeInBack",
 "easeOutBack",
 "easeInOutBack",
@@ -31,21 +31,75 @@ var easingsList = [
 "easeOutBounce"
 ];
 
+
 var moveRight = function(){
+    var d = parseInt($("#delay").val());
+    var n = 0;
+    while (n <= easingsList.length)
+    {
+        var e = easingsList[n];
+        moveBoxRight(n, e, d);
+        n++;
+    }
 }
 
 var moveLeft = function(){
+    var d = parseInt($("#delay").val());
+    var n = 0;
+    while(n <= easingsList.length)
+    {
+        var e = easingsList[n];
+        moveBoxLeft(n, e ,d);
+        n++;
+    }
 }
 
-var moveBoxLeft= function(n, easing, endcolor, duration)
+var moveBoxRight = function(n, easing, duration)
+{    var id = "#button" + n.toString();
+     var pageWidth = $("body").width();
+     var boxWidth = 150;
+     $(id).animate({"margin-left" : pageWidth-boxWidth + "px"}, duration, easing);
+}
+
+var moveBoxLeft = function(n, easing, duration)
 {
   var id = "#button" + n.toString();
   var pageWidth = $("body").width();
   var boxWidth = 150;
-  if(boxStatus[n] == "left"){
-    $(id).animate({"margin-left" : pageWidth - boxWidth + "px", "color": endcolor}, duration, easing); 
-  }
-  else{
-    $(id).animate({"margin-left" : "0px", "color" : endColor}, duraion, easing); 
-  }
+  $(id).animate({"margin-left" : "0px"}, duration, easing); 
 }
+
+var showBoxes = function(){
+    var selection = parseInt($(".button").val();
+
+
+
+var hideBoxes = function(){
+    var selection = parseInt($("#jqoption").val())
+    if(selection == 0 ){
+        $(".button").fadeOut();
+    }
+    else if(selection == 1){
+        $(".button").fadeToggle();
+    }
+    else if(selection == 2){
+        $(".button").slideToggle();
+    }
+    else if(selection == 3){
+        $(".button").slideUp();
+    $(".button").animate("visibility: hidden");
+}
+
+var toggleBoxes = function(){
+    var selection = $("#hideShowButton").html();
+    if(selection == "Hide Boxes"){
+        hideBoxes();
+        $("#hideShowButton").html("Show Boxes");
+    }
+    else{
+        showBoxes();
+        $("#hideBoxes").html("Hide Boxes");
+    }
+
+}
+
